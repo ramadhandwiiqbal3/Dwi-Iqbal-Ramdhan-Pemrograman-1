1a39e729b7304ae7231e4269ddd043e5f1423e83
ramadhandwiiqbal3 <ramadhandwiiqbal3@gmail.com>
2026-04-18 20:29:06 +0700

    Tugas P7

--- minggu_7/source package/latihan/main.java
+++ minggu_7/source package/latihan/main.java
@@ -1,6 +1,24 @@
-
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Piliahan : ");
+        int Pilih = input.nextInt();
+        input.nextLine();
+        
+        if(Pilih == 1){
+            System.out.println("Masukan Nama : ");
+            String nama = input.nextLine();
+            
+            System.out.println("Masukan Jurusan");
+            String Jurusan = input.nextLine();
+            System.out.println("Nama : " + nama);
+            System.out.println("Jurusan : " + Jurusan);
+            }else {
+            System.out.println("Pilihan Mu Adalah : " + Pilih);
+            }
+            System.out.println("System Selsesai.");
+            
+        input.close();
     }
 }
